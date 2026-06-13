14d776e30d5325523eb5fe34a34a8c6ed72401b6
EijiMoriyama1302 <e-moriei.305@ezweb.ne.jp>
2026-06-13 10:42:18 +0900

    [GREEN]add shared_memory_pool

--- my_api.cpp
+++ my_api.cpp
@@ -1,6 +1,14 @@
 #include "my_api.h"
 
 void MyApi::mp_api_init() {
+    // 1. 2MB × 8個 = 16MB のメモリを動的に一括確保
+    shared_memory_pool.resize(BUFFER_SIZE * BUFFER_COUNT);
+
+    // 2. 各バッファのアドレスを割り振り、初期状態としてすべて「空」キューに入れる
+    for (size_t i = 0; i < BUFFER_COUNT; ++i) {
+        buffers[i] = &shared_memory_pool[i * BUFFER_SIZE];
+    }
+
     // th_demux スレッドを起動
     demux_thread = std::thread(&MyApi::th_demux, this);
 }
